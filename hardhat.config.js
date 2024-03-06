b96710a8b3ece21419fbf1ba7cdf4496fd7a496c
Jason Yapri <jason.yapri@gmail.com>
2024-03-06 22:45:16 +0700

    using ethereum sepolia test network and alchemy api key on hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const ALCHEMY_API_KEY = "https://eth-sepolia.g.alchemy.com/v2/[your alchemy api key here]";
+const SEPOLIA_PRIVATE_KEY = "[your private key here]";
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: ALCHEMY_API_KEY,
+      accounts: [`0x${SEPOLIA_PRIVATE_KEY}`]
+    }
+  }
 };
